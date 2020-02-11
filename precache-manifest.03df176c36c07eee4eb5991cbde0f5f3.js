255fd4be8f271024db25eeefb3710c5dd8e5e5cc
unknown <miroslavigpm@gmail.com>
2020-02-11 20:05:06 +0100

    Updates

---
+++ precache-manifest.03df176c36c07eee4eb5991cbde0f5f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fd31ed7bf3c227c321489dda5c3d4f",
+    "url": "/Movie-database-redux/index.html"
+  },
+  {
+    "revision": "09b1f65782eba6bbc6fd",
+    "url": "/Movie-database-redux/static/css/main.9b28dc0e.chunk.css"
+  },
+  {
+    "revision": "b121d531b8f2519afd05",
+    "url": "/Movie-database-redux/static/js/2.bd8dc95e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Movie-database-redux/static/js/2.bd8dc95e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09b1f65782eba6bbc6fd",
+    "url": "/Movie-database-redux/static/js/main.e4e57216.chunk.js"
+  },
+  {
+    "revision": "0f002289d22e4b2f4b61",
+    "url": "/Movie-database-redux/static/js/runtime-main.f49c902c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Movie-database-redux/precache-manifest.03df176c36c07eee4eb5991cbde0f5f3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Movie-database-redux/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
